ba8b1bf4caf93dd67f12d75f20e9037e1bd36242
mckernan28 <jmsmckernan0@gmail.com>
2016-09-29 21:53:26 -0400

    changed to php

---
+++ settings.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTf-8">
+    <title>James Mckernan</title>
+    <link rel="stylesheet" href="style.css"/>
+    <script src="jquery3.1.0.js"></script>
+    <script src="script.js"></script>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="icon"
+          type="image/png"
+          href="MinecraftHeads/mckernan28-avatar.png">
+    <link rel="apple-touch-icon" href="MinecraftHeads/mckernan28-avatar.png"/>
+    <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="banner-container">
+        <div class="banner-container-left">
+            <a class="logo-link" href="index.php">
+                <img src="logo-small.png" alt="" class="banner-logo">
+            </a>
+        </div>
+        <div class="banner-container-right">
+            <p class="banner-phrase">THE ONE AND ONLY!</p>
+        </div>
+    </div>
+
+    <div class="menu-bar pic-menu">
+        <div class="menu-bar-left-container">
+
+        </div>
+        <div class="menu-bar-right-container">
+            <div class="hamburger-container">
+                <a class="hamburger" href="#">
+                    <div class="bun top"></div>
+                    <div class="meat"></div>
+                    <div class="bun bottom"></div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="dropdown-menu-container">
+        <div class="dropdown-menu">
+            <div class="profile-overview-menu-settings-container">
+                <div class="profile-overview-menu-settings">
+                    <div class="img-container">
+                        <img class="profile-picture-menu-settings" src="mckernan28-avatar.png" alt="">
+                        <a class="profile-name-menu-settings" href="#">James Mckernan</a>
+                        <a class="profile-username-menu-settings" href="#">@Mckernan28</a>
+                    </div>
+                </div>
+            </div>
+            <div class="settings-menu-settings-container">
+                <div class="dropdown-menu-settings">
+                    <ul class="dropdown-menu-settings-link-container">
+                        <li>
+                            <a class="dropdown-menu-settings-link" href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-menu-settings-link" href="settings.php">Settings</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-menu-settings-link" href="#">Help</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-menu-settings-link" href="#">Log Out</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-container">
+        <div class="banner-container-compensator"></div>
+        <div class="settingspage">
+
+            <div class="left-column">
+                <div class="profile-overview-settings-container">
+                    <div class="profile-overview-settings">
+                        <p class="setting-headers">Profile Overview</p>
+                        <div class="profile-overview-settings-picture-container">
+                            <img src="mckernan28-avatar.png" alt="profile-picture" class="profile-overview-settings-picture">
+                        </div>
+                        <a class="profile-overview-name" href="">James Mckernan</a>
+                        <a class="profile-overview-username" href="">@mckernan28</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="right-column">
+                <div class="profile-settings-container">
+                    <div class="profile-settings">
+                        <p class="setting-headers">Settings</p>
+                        <form id="settings-form" action="">
+                            <div id="edit-username-container">
+                                <label id="name-change">Username:</label>
+                                <p data-editable id="current-username">@mckernan28</p>
+                                <div id="edit-container">
+                                    <i class="fa fa-pencil edit" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <div id="username-edit-input">
+                                <input class="btn save-button" type="submit" value="Save">
+                                <input id="cancel-username-edit" class="btn cancel-button" type="button" value="Cancel">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+    </div>
+
+    <div class="footer-container">
+        <div class="footer">
+            <p class="footer-text"> Copyright 2016 by James Mckernan. All Rights Reserved. Powered by &copy MrMckernan28.</p>
+        </div>
+    </div>
+</body>
+</html>
