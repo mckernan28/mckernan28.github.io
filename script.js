7334245ba164bd10cafdd2a6b7410ec391125419
James Mckernan <Mckernan28@Jamess-MacBook-Pro.local>
2016-10-07 15:25:23 -0400

    Edit Description and Username is functional

--- script.js
+++ script.js
@@ -14,40 +14,44 @@ $(document).ready(function() {
         }
     });
 
-    //Save and Cancel functions and animation for Nickname on settings page
+    //When any 'edit' button is clicked
+    $('.edit').click(function (e) {
+        e.preventDefault();
+        var profileSettingsHeight = $(".profile-settings").height();
+        // $('.profile-settings').animate({height: profileSettingsHeight + 90},300);
+        $("#username-edit-input").fadeIn(200);
+    })
+
+    //When edit Description button is clicked
     $("#edit-description-button").click(function(e){
         var profileDescriptionHeight = $("#current-description").height();
-        var $input = $('<input class="inputs" id="description-input"/>');
-
-        $('#current-description').replaceWith( $input );
-        $('#description-input').height(profileDescriptionHeight)
-    });
-    $("#cancel-description-edit").click(function(e){
-        var $p = $('<p data-editable id="current-description">Hello my name is James Mckernan I am 16 years old and I am in Sec. 5 at Heritage Regional Highschool. I am enrolled in a computer programming course called iCan for my fifth year in 2016-2017</p>');
-        var profileSettingsHeight = $(".profile-settings").height();
-
+        var $inputDescription = $('<textarea class="inputs" id="description-input"/>');
+        $('#current-description').replaceWith( $inputDescription );
+        $('#description-input').outerHeight(profileDescriptionHeight)
+        $("#edit-description-button").hide();
         e.preventDefault();
-        $('#username-input').replaceWith( $p );
     });
 
-    //Save and Cancel functions and animation for Nickname on settings page
+    //When edit Username button is clicked
     $("#edit-username").click(function(e){
         var $input = $('<input class="inputs" id="username-input"/>');
-        var profileSettingsHeight = $(".profile-settings").height();
-        e.preventDefault();
-        $('.profile-settings').animate({height: profileSettingsHeight + 50},300);
-        $(this).toggle();
-        $('#username-edit-input').fadeToggle(300);
         $('#current-username').replaceWith( $input );
-    });
-    $("#cancel-username-edit").click(function(e){
-        var $p = $('<p data-editable id="current-username">@mckernan28</p>');
-        var profileSettingsHeight = $(".profile-settings").height();
+        $(this).hide();
         e.preventDefault();
-        $('.profile-settings').animate({height: profileSettingsHeight - 50},300);
-        $("#edit-username").toggle();
-        $('#username-edit-input').fadeToggle(300);
-        $('#username-input').replaceWith( $p );
+    });
+
+    //When cancel button is click
+    $("#cancel-settings-edit").click(function(e){
+        var $pUsername = $('<p data-editable id="current-username">@mckernan28</p>');
+        var profileSettingsHeight = $(".profile-settings").height();
+        var $pDescription = $('<p data-editable id="current-description">Hello my name is JameHello my name is James Mckernan I am 16 years old and I am in Sec. 5 at Heritage Regional Highschool. I am enrolled in a computer programming course called iCan for my fifth year in 2016-2017s Mckernan I am 16 years old and I am in Sec. 5 at Heritage Regional Highschool. I am enrolled in a computer programming course called iCan for my fifth year in 2016-2017</p>');
+        // $('.profile-settings').animate({height: profileSettingsHeight - 90},300);
+        $("#edit-description-button").show();
+        $("#edit-username").show();
+        $('#description-input').replaceWith( $pDescription );
+        $('#username-input').replaceWith( $pUsername );
+        $("#username-edit-input").fadeOut(200);
+        e.preventDefault();
     });
 
     //Toggles menu icon in menu-bar for navigation
--- settings.php
+++ settings.php
@@ -97,20 +97,18 @@
                         <div class="setting-headers-container">
                             <p class="setting-headers">Settings</p>
                         </div>
-                        <form id="settings-form" action="">
+                        <form id="settings-form" action="" onsubmit="return false;">
                             <div id="edit-profile-picture-container">
                                 <p class="settings-titles">Profile Picture:</p>
                                 <img src="mckernan28-avatar.png" alt="Preview" id="profile-preview-settings-picture">
-                                <div id="edit-profile-picture-button-container">
-                                    <div class="btn" id="edit-profile-picture-button">edit <i class="fa fa-camera" aria-hidden="true"></i></div>
-                                </div>
+                                <div class="btn" id="edit-profile-picture-button">edit <i class="fa fa-camera" aria-hidden="true"></i></div>
                             </div>
 
                             <div id="edit-description-container">
                                 <p class="settings-titles">Description:</p>
-                                <p data-editable id="current-description">Hello my name is James Mckernan I am 16 years old and I am in Sec. 5 at Heritage Regional Highschool. I am enrolled in a computer programming course called iCan for my fifth year in 2016-2017</p>
+                                <p data-editable id="current-description">Hello my name is JameHello my name is James Mckernan I am 16 years old and I am in Sec. 5 at Heritage Regional Highschool. I am enrolled in a computer programming course called iCan for my fifth year in 2016-2017s Mckernan I am 16 years old and I am in Sec. 5 at Heritage Regional Highschool. I am enrolled in a computer programming course called iCan for my fifth year in 2016-2017</p>
                                 <div id="edit-container">
-                                    <button type="button" class="btn" id="edit-description-button">edit</button>
+                                    <button type="button" class="btn edit" id="edit-description-button">edit</button>
                                 </div>
                             </div>
 
@@ -118,13 +116,13 @@
                                 <p class="settings-titles">Username:</p>
                                 <p data-editable id="current-username">@mckernan28</p>
                                 <div id="edit-username-container">
-                                    <i class="fa fa-pencil" id="edit-username" aria-hidden="true"></i>
+                                    <i class="fa fa-pencil edit" id="edit-username" aria-hidden="true"></i>
                                 </div>
                             </div>
 
                             <div id="username-edit-input" class="hidden">
                                 <input class="btn save-button" type="submit" value="Save">
-                                <input id="cancel-username-edit" class="btn cancel-button" type="button" value="Cancel">
+                                <input id="cancel-settings-edit" class="btn cancel-button" type="button" value="Cancel">
                             </div>
                         </form>
                     </div>
