81ecd17dfe0d788620fc05c8f258f3d2d85ecd1a
mckernan28 <jmsmckernan0@gmail.com>
2017-01-18 22:32:23 -0500

    incoming

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    var topOfOthDiv = $(".banner-container").outerHeight();
+
+    $(".hamburger").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('hamburger-active');
+        $('.hamburger-container').toggleClass('margin-adjust-menu');
+        $('.dropdown-menu-container').toggleClass('dropdown-menu-container-active');
+    });
+
+    $(".square").click(function(e){
+        e.preventDefault();
+        if ($(this).hasClass('square-active')) {
+            $(this).toggleClass('square-active');
+        }
+        else {
+            $(".square").removeClass('square-active');
+            $(this).addClass('square-active');
+        }
+    });
+
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) {
+            $('.menu-bar').addClass("menu-bar-active");
+            $('.dropdown-menu-container').addClass("dropdown-menu-container-activated");
+            $('.banner-container-compensator').addClass('banner-container-compensator-active');
+        }else{
+            $('.menu-bar').removeClass("menu-bar-active");
+            $('.dropdown-menu-container').removeClass("dropdown-menu-container-activated");
+            $('.banner-container-compensator').removeClass('banner-container-compensator-active');
+        }
+    });
+});
