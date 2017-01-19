24ff2a2e85d185e9e0d28e93129223b523827a12
mckernan28 <jmsmckernan0@gmail.com>
2017-01-18 22:35:49 -0500

    Here it is

--- otherwebsite/script.js
+++ otherwebsite/script.js
@@ -1,33 +1,43 @@
-$(document).ready(function() {
-    var topOfOthDiv = $(".banner-container").outerHeight();
-
-    $(".hamburger").click(function(e){
-        e.preventDefault();
-        $(this).toggleClass('hamburger-active');
-        $('.hamburger-container').toggleClass('margin-adjust-menu');
-        $('.dropdown-menu-container').toggleClass('dropdown-menu-container-active');
-    });
-
-    $(".square").click(function(e){
-        e.preventDefault();
-        if ($(this).hasClass('square-active')) {
-            $(this).toggleClass('square-active');
-        }
-        else {
-            $(".square").removeClass('square-active');
-            $(this).addClass('square-active');
-        }
-    });
-
-    $(window).scroll(function() {
-        if($(window).scrollTop() > topOfOthDiv) {
-            $('.menu-bar').addClass("menu-bar-active");
-            $('.dropdown-menu-container').addClass("dropdown-menu-container-activated");
-            $('.banner-container-compensator').addClass('banner-container-compensator-active');
-        }else{
-            $('.menu-bar').removeClass("menu-bar-active");
-            $('.dropdown-menu-container').removeClass("dropdown-menu-container-activated");
-            $('.banner-container-compensator').removeClass('banner-container-compensator-active');
-        }
+// Execute when document is ready
+$(document).ready(() => {
+    // Animate the page to the top after a refresh
+    $('body').animate({scrollTop: 0}, "fast", () => {
+        // Timeout of 4s
+        setTimeout(() => {
+            // Variables for duplicate selectors
+            const arrows = $('.arrow-container');
+            const whiteBar = $('.white-bar');
+            const subtitleContainer = $('.subtitle-container');
+            // Make object now clickable
+            $(arrows).addClass('clickable');
+            $(whiteBar).addClass('clickable');
+            // Scroll down until whiteBar is in the middle when 'arrows' is clicked
+            $(arrows).click(() => {
+                // Scroll to the 'whiteBar' position from top and subtract half of it's height
+                $('body').animate({scrollTop: whiteBar.offset().top - whiteBar.outerHeight() / 2}, 'slow');
+            });
+            // Animate the page back to top
+            $('body').animate({scrollTop: 0}, 'fast', () => {
+                // Function for the scrolling of the page
+                $(window).scroll(() => {
+                    if (subtitleContainer) {
+                        // Get the height of browser page
+                        const windowHeight = window.outerHeight;
+                        // Variable with distance scrolled from top
+                        const scrolled = $('body').scrollTop() / windowHeight;
+                        // Opacity changes with the distance scrolled
+                        const opacitySubtitle = Math.max(Math.min(1 - (scrolled * 2), 1), 0);
+                        const opacityWhiteBar = Math.max(Math.min((scrolled - 0.35) * 1.5, 1), 0);
+                        // Change the opacity of elements depending on the distance scrolled
+                        subtitleContainer.css({'opacity': opacitySubtitle});
+                        whiteBar.css({'opacity': opacityWhiteBar});
+                    }
+                });
+            });
+            // Called when 'whiteBar' is clicked
+            $(whiteBar).click(() => {
+                whiteBar.addClass('white-bar-active');
+            });
+        }, 4000)
     });
 });
